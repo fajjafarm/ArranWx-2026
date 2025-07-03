2d3e4d6b523dc31c673f00463502a6661f248e18
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-07-03 19:19:04 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,12 @@ use App\Http\Controllers\RoutingController;
 |
 */
 
-require __DIR__ . '/auth.php';
 
-Route::group(['prefix' => '/', 'middleware' => 'auth'], function () {
     Route::get('', [RoutingController::class, 'index'])->name('root');
     Route::get('/home', fn()=>view('dashboards.index'))->name('home');
     Route::get('{first}/{second}/{third}', [RoutingController::class, 'thirdLevel'])->name('third');
     Route::get('{first}/{second}', [RoutingController::class, 'secondLevel'])->name('second');
     Route::get('{any}', [RoutingController::class, 'root'])->name('any');
-});
+
 
         
