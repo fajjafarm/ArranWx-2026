d41cd9deee89bdfe2392e5d2b1bc2fe547391ba3
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-07-04 15:01:58 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WeatherController;
 use App\Http\Controllers\ResourcesController;
 use App\Http\Controllers\RoutingController;
 
