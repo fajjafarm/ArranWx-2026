db24754181ef83342fd5b76025e41e0ae9ac29c2
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-07-04 15:25:09 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WeatherController;
 
+
+// Homepage route to render index.blade.php
+Route::get('/', [WeatherController::class, 'index'])->name('index');
+
 Route::get('/locations', function () {
     return App\Models\Location::all();
 });
 
 Route::get('/weather', [WeatherController::class, 'getWeather']);
+
