4ba38c506e2c5a59995753aaec1f4791de0b1f8e
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-07-05 18:16:44 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WeatherController;
 
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application.
+|
+*/
+
 Route::get('/locations', function () {
     return App\Models\Location::all();
 });
