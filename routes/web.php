f81161510d64cd77461cdef2faee7becd0b38823
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-07-05 18:57:18 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,8 @@ Route::get('/', [WeatherController::class, 'index'])->name('dashboards.index');
 
 // Dynamic location route
 Route::get('/forecast/{slug}', [WeatherController::class, 'indexBySlug'])->name('forecast');
+
+// Catch-all routes (must be last)
+Route::get('{first}/{second}/{third}', [RoutingController::class, 'thirdLevel'])->name('third');
+Route::get('{first}/{second}', [RoutingController::class, 'secondLevel'])->name('second');
+Route::get('{any}', [RoutingController::class, 'root'])->name('any');
